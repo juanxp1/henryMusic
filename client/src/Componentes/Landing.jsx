97a025a69caeaad65f73ca04b4b2adf7cbb7f95d
Ayamatt <dagracacarlos16@gmail.com>
2023-01-25 12:58:22 -0300

    testing

--- client/src/Componentes/Landing.jsx
+++ client/src/Componentes/Landing.jsx
@@ -9,6 +9,8 @@ function Landing() {
     return (
         <div className="contianer">
             <h1 className='container'>Bienvenidos a Henry Music</h1>
+            <h2> HOLAAAAA </h2>
+            <h3> probando </h3>
             <Login />
         </div>
     )
