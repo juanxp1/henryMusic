f5c75a8ca1dde3bb17c287594a90fc9903b10b1f
Ayamatt <dagracacarlos16@gmail.com>
2023-02-05 13:37:55 -0300

    cambio en el nombre para que pueda importarse

--- client/src/Store/store.js
+++ client/src/Store/store.js
@@ -1,5 +1,5 @@
 import {createStore, applyMiddleware, compose } from 'redux';
-import reducer from '../reducer/reducer';
+import reducer from '../Reducer/reducer';
 import thunk from 'redux-thunk';
 
 const composeEnhancers = compose;
