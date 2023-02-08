dfca5dde332fad2ed90b59a4a009129be4955693
Juan Jose Bianchi <juan-josebianchi@hotmail.com>
2023-02-08 12:30:36 -0300

    modificacion nave

--- client/src/Componentes/Nave/Nave.jsx
+++ client/src/Componentes/Nave/Nave.jsx
@@ -7,19 +7,7 @@ import Login from '../Login/Login'
 import { useAuth0 } from "@auth0/auth0-react";
 import styled from 'styled-components'
 import { Link } from "react-router-dom"
-import AppBar from '@mui/material/AppBar';
-import Box from '@mui/material/Box';
-import Toolbar from '@mui/material/Toolbar';
-import IconButton from '@mui/material/IconButton';
-import Typography from '@mui/material/Typography';
-import Menu from '@mui/material/Menu';
-import MenuIcon from '@mui/icons-material/Menu';
-import Container from '@mui/material/Container';
-import Avatar from '@mui/material/Avatar';
-import Button from '@mui/material/Button';
-import Tooltip from '@mui/material/Tooltip';
-import MenuItem from '@mui/material/MenuItem';
-import AdbIcon from '@mui/icons-material/Adb';
+
 
 const pages = ['Products', 'Pricing', 'Blog'];
 const settings = ['Profile', <Login />, 'Dashboard', <Logout />];
@@ -27,23 +15,7 @@ const settings = ['Profile', <Login />, 'Dashboard', <Logout />];
 
 function Nave() {
 
-  const [anchorElNav, setAnchorElNav] = React.useState(null);
-  const [anchorElUser, setAnchorElUser] = React.useState(null);
 
-  const handleOpenNavMenu = (event) => {
-    setAnchorElNav(event.currentTarget);
-  };
-  const handleOpenUserMenu = (event) => {
-    setAnchorElUser(event.currentTarget);
-  };
-
-  const handleCloseNavMenu = () => {
-    setAnchorElNav(null);
-  };
-
-  const handleCloseUserMenu = () => {
-    setAnchorElUser(null);
-  };
 
   const { user, isAuthenticated, isLoading } = useAuth0();
 
