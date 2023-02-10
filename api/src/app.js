cd7c92a0f761f0d2c6e1b0285bed276b1670aadb
antomuelle <mguzman.muelle@gmail.com>
2023-02-10 17:22:49 -0500

    making authentication

--- api/src/app.js
+++ api/src/app.js
@@ -3,7 +3,6 @@ import express from 'express';
 import cookieParser from 'cookie-parser';
 import morgan from 'morgan';
 import routes from './routes/index.js';
-import { auth } from 'express-openid-connect'
 
 const server = express();
 // ruta donde se guardaran todos los archivos que se suban al servidor, ex: D:\proyectos\henry\public
@@ -35,4 +34,48 @@ server.use((err, req, res, next) => { // eslint-disable-line no-unused-vars
   res.status(status).send(message);
 });
 
+// Auth0 section ---------------------------------------------------------
+import expressSession from 'express-session'
+import passport from 'passport'
+import ESS from 'express-session-sequelize'
+
+const SessionStore = ESS(expressSession.Store)
+
+server.use(expressSession({
+  secret: process.env.SESSION_SECRET,
+  resave: false,
+  saveUninitialized: true,
+  store: new SessionStore({ db: sequelize }),
+}))
+
+
 export default server
+
+/*
+import { expressjwt } from 'express-jwt'
+import jwksRsa from 'jwks-rsa'
+
+const checkJwt = expressjwt({
+  secret: jwksRsa.expressJwtSecret({
+    cache: true,
+    rateLimit: true,
+    jwksRequestsPerMinute: 5,
+    jwksUri: 'https://dev-1qthsdxhtwz6pywj.us.auth0.com/.well-known/jwks.json'
+  }),
+  audience: 'https://henrymusic.tech',
+  issuer: 'https://dev-1qthsdxhtwz6pywj.us.auth0.com/',
+  algorithms: ['RS256']
+});
+
+server.get('/api/public', function(req, res) {
+  res.json({
+    message: 'Hello from a public endpoint! You don\'t need to be authenticated to see this.'
+  });
+});
+
+server.get('/api/private', checkJwt, function(req, res) {
+  res.json({
+    message: 'Hello from a private endpoint! You need to be authenticated to see this.'
+  });
+});
+*/
