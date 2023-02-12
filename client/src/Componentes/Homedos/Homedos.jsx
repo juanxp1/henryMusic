1fb8746d9b07b1dbde09bfedd058864fdacc1244
Agustín Guerrero <agustinguerrero642@gmail.com>
2023-02-12 14:12:11 -0300

    Loading

--- client/src/Componentes/Homedos/Homedos.jsx
+++ client/src/Componentes/Homedos/Homedos.jsx
@@ -37,7 +37,23 @@ const Homedos = () => {
     return (
 
         <Container >
+
+            
+                
+
             <div className="contenedor ">
+
+                <div className='uwu'>
+                    <div className="loaderRectangle">
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                    </div>
+                </div>
+
+
                 <div className='container-fluid'>
                     <h1 className='h1 '>¡Buenos días! <span className='pit'>{user?.nickname.toUpperCase()}</span>  </h1>
                     <h2 className='d-flex justify-content-start h1'>Top artistas </h2>
@@ -201,19 +217,35 @@ li{
 
 
 .contenedor{
-
+    
     width: auto;
-    height: auto;
+    height: 100vh;
     background: rgb(0,0,0);
     background: linear-gradient(124deg, rgba(0,0,0,1) 5%, rgba(53,24,74,1) 100%, rgba(63,28,87,1) 100%, rgba(91,40,125,1) 100%, rgba(131,58,180,1) 100%);
     margin-left: 230px  !important;
     color: white;
     display: flex;
     position: relative;
-    padding-bottom: 50px;
+    padding-bottom: 0px;
 
     
 }
+.container-fluid{
+    animation-name: container-fluid;
+    animation-duration: 7s;
+}
+@keyframes container-fluid{
+    0% {
+        opacity: 0;
+      }
+      90% {
+        opacity: 0;
+      }
+      100% {
+        opacity: 1;
+      }
+}
+
 
 .btn-wrapper {
     border-radius: 10px;
@@ -259,6 +291,138 @@ li{
     }
   }
 
-
+  .uwu{
+    animation-name: uwu;
+    animation-duration: 10s;
+    opacity: 0;
+    position: absolute;
+    top: 300px;
+    left: 550px;
+    display:flex;
+    justify-content: center;
+    align-items: center;
+  }
+  @keyframes uwu{
+    0% {
+        opacity: 1;
+      }
+    
+      75% {
+        opacity: 0;
+      }
+    
+      100% {
+        opacity: 0;
+      }
+  }
+  .loaderRectangle {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    gap: 0 3px;
+   }
+   
+   .loaderRectangle div {
+    width: 10px;
+    height: 16px;
+    animation: .9s ease-in-out infinite;
+    background: linear-gradient(270deg, yellow, white, orange, red);
+    box-shadow: 0 0 20px rgba(18, 31, 53, 0.3);
+   }
+   
+   .loaderRectangle div:nth-child(1) {
+    animation-name: rectangleOneAnim;
+    animation-delay: 1s;
+   }
+   
+   @keyframes rectangleOneAnim {
+    0% {
+     height: 15px;
+    }
+   
+    40% {
+     height: 30px;
+    }
+   
+    100% {
+     height: 15px;
+    }
+   }
+   
+   .loaderRectangle div:nth-child(2) {
+    animation-name: rectangleTwoAnim;
+    animation-delay: 1.1s;
+   }
+   
+   @keyframes rectangleTwoAnim {
+    0% {
+     height: 15px;
+    }
+   
+    40% {
+     height: 40px;
+    }
+   
+    100% {
+     height: 15px;
+    }
+   }
+   
+   .loaderRectangle div:nth-child(3) {
+    animation-name: rectangleThreeAnim;
+    animation-delay: 1.2s;
+   }
+   
+   @keyframes rectangleThreeAnim {
+    0% {
+     height: 15px;
+    }
+   
+    40% {
+     height: 50px;
+    }
+   
+    100% {
+     height: 15px;
+    }
+   }
+   
+   .loaderRectangle div:nth-child(4) {
+    animation-name: rectangleFourAnim;
+    animation-delay: 1.3s;
+   }
+   
+   @keyframes rectangleFourAnim {
+    0% {
+     height: 15px;
+    }
+   
+    40% {
+     height: 40px;
+    }
+   
+    100% {
+     height: 15px;
+    }
+   }
+   
+   .loaderRectangle div:nth-child(5) {
+    animation-name: rectangleFiveAnim;
+    animation-delay: 1.4s;
+   }
+   
+   @keyframes rectangleFiveAnim {
+    0% {
+     height: 15px;
+    }
+   
+    40% {
+     height: 30px;
+    }
+   
+    100% {
+     height: 15px;
+    }
+   }
 
 `
