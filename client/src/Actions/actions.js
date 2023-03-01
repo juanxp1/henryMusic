fff583e0f9b2c4067b5e19f73d2e773f1b310a07
Ayamatt <dagracacarlos16@gmail.com>
2023-03-01 15:14:14 -0300

    ordenando actions

--- client/src/Actions/actions.js
+++ client/src/Actions/actions.js
@@ -1,35 +1,67 @@
 import axios from 'axios';
 
-const URL = process.env.NODE_ENV === 'production' ? 'https://henrymusic.tech/api' : 'http://localhost:3001/api';
+// VARIABLES
 
-// Actions de nuestra base de datos
+const URL = process.env.NODE_ENV === 'production' ? 'https://henrymusic.tech/api' : 'http://localhost:3001/api';
+const initialLimit = 10;
+
+// ------------------------------------------- ACTIONS TYPE NAMES ----------------------------------------------
+
+// TOKEN
 
 export const GET_TOKEN = 'GET_TOKEN';
+
+// USER
+
+export const GET_USER = "GET_USER;"
+export const UPDATEMYUSER = 'UPDATEMYUSER';
+
+// TRACKS
+
 export const GET_TRACK = 'GET_TRACK';
 export const SEARCH_TRACK = 'SEARCH_TRACK';
+export const GET_ALL_TRACKS = 'GET_ALL_TRACKS';
+
+// ALBUMS
+
 export const GET_ALBUM = 'GET_ALBUM';
 export const SEARCH_ALBUM = 'SEARCH_ALBUM';
+export const GET_ALL_ALBUMS = 'GET_ALL_ALBUMS';
+
+// ARTISTS
+
 export const GET_ARTIST = 'GET_ARTIST';
 export const SEARCH_ARTIST = 'SEARCH_ARTIST';
-export const GET_ALL_TRACKS = 'GET_ALL_TRACKS';
-export const GET_ALL_ALBUMS = 'GET_ALL_ALBUMS';
 export const GET_ALL_ARTISTS = 'GET_ALL_ARTISTS';
-export const FILTRO_GENERO = 'FILTRO_GENERO';
-export const RESET_DETALLES = "RESET_DETALLES;"
-export const GET_PLAYER = "GET_PLAYER;"
-export const IS_PLAYING = 'IS_PLAYING';
-export const GET_USER = "GET_USER;"
-export const ORDEN_BY_NAME = "ORDEN_BY_NAME;"
+
+// PLAYLISTS
+
 export const GET_ALL_PLAYLISTS = 'GET_ALL_PLAYLISTS';
 export const GET_ALL_PLAYLIST_TRACKS = 'GET_ALL_PLAYLIST_TRACKS';
-export const UPDATEMYUSER = 'UPDATEMYUSER';
+
+// NEW SONG
 
 export const GET_NEW_SONG = "GET_NEW_SONG";
 export const DELETE_SONG = "DELETE_SONG";
 
+// FILTROS
+
+export const FILTRO_GENERO = 'FILTRO_GENERO';
+export const ORDEN_BY_NAME = "ORDEN_BY_NAME;"
+
+// REPRODUCTOR
+
+export const GET_PLAYER = "GET_PLAYER;"
+export const IS_PLAYING = 'IS_PLAYING';
+
+// OTROS
+
+export const RESET_DETALLES = "RESET_DETALLES;"
 
 
-const initialLimit = 10;
+// ------------------------------------------- ACTIONS ----------------------------------------------
+
+// ------------------ TOKEN --------------------------------------
 
 export const getToken = (token) => {
     return {
@@ -37,6 +69,25 @@ export const getToken = (token) => {
         payload: token
     }
 }
+
+// ------------------ USER --------------------------------------
+
+export const getUser = () => {
+    return async function(dispatch) {
+        const info = await axios.get(`${URL}/user`)
+        return dispatch({type: GET_USER, payload: info.data})
+    }
+}
+
+
+export const updateMyUser = (username, image) => {
+    return async function() {
+        const info = await axios.post(`${URL}/user/update`, {username, image}, {headers: {"Content-Type": "multipart/form-data"}})
+        return info;
+    }
+}
+
+// ------------------ TRACKS --------------------------------------
 
 export const getTrack = (ID) => {
     return async function (dispatch) {
@@ -59,6 +110,8 @@ export const getAllTracks = (limit = initialLimit) => {
     };
 };
 
+// ------------------ ALBUMS --------------------------------------
+
 export const getAlbum = (ID) => {
     return async function (dispatch) {
         const info = await axios.get(`${URL}/album/${ID}`)
@@ -73,21 +126,14 @@ export const searchAlbum = (album, limit = initialLimit) => {
     };
 };
 
-
-export const getNewSong = () =>{
-    return async function(dispatch){
-        const info = await axios.get(`${URL}/song/all`)
-        return dispatch({ type: GET_NEW_SONG, payload: info.data })
-    }
-}
-
-
 export const getAllAlbums = (limit = initialLimit) => {
     return async function (dispatch) {
         const info = await axios.get(`${URL}/album/all?limit=${limit}`)
         return dispatch({ type: GET_ALL_ALBUMS, payload: info.data })
     };
 };
+
+// ------------------ ARTISTS --------------------------------------
 
 export const getArtist = (ID) => {
     return async function (dispatch) {
@@ -112,74 +158,7 @@ export const getAllArtists = (limit = initialLimit) => {
 
 };
 
-export const postSong = (song) => {
-    return async function () {
-        let json = await axios.post(`${URL}/song/create`, song, {headers: {'Content-Type': 'multipart/form-data'}})
-        return json;
-    }
-}
-export const deleteSong = (ID) => {
-    
-    return async function () {
-        let json = await axios.post(`${URL}/song/delete`, {id:ID})
-        return json;
-    }
-}
-
-
-export const update = (user) => {
-    return async function () {
-        let json = await axios.put(`${URL}`, user)
-        return json;
-    }
-}
-
-export const Landing = () => {
-    return {
-        type: "LANDING"
-    }
-}
-
-  //reset  detail
-export function resetDetalles() {
-    return async function (dispatch) {
-        dispatch({ type: RESET_DETALLES })
-    }
-}
-
-export const filtroGenero = (payload) => {
-    return {
-        type: FILTRO_GENERO,
-        payload
-    }
-}
-
-export const getPlayer = (payload) => {
-    return {
-        type: GET_PLAYER,
-        payload
-    }
-}
-
-export const isPlaying = () => {
-    return {
-        type: IS_PLAYING
-    }
-}
-
-export const getUser = () => {
-    return async function(dispatch) {
-        const info = await axios.get(`${URL}/user`)
-        return dispatch({type: GET_USER, payload: info.data})
-    }
-}
-
-export const ordenPorAbc = (payload) => {
-    return {
-        type: ORDEN_BY_NAME,
-        payload
-    }
-}
+// ------------------ PLAYLISTS --------------------------------------
 
 export const getAllPlaylists = (limit = initialLimit) => {
     return async function (dispatch) {
@@ -209,13 +188,77 @@ export const playlistDeleteTrack = (playlist_id, track_id) => {
     };
 }
 
+// ------------------ NEW SONG --------------------------------------
 
-export const updateMyUser = (username, image) => {
-    return async function() {
-        const info = await axios.post(`${URL}/user/update`, {username, image}, {headers: {"Content-Type": "multipart/form-data"}})
-        return info;
+export const getNewSong = () =>{
+    return async function(dispatch){
+        const info = await axios.get(`${URL}/song/all`)
+        return dispatch({ type: GET_NEW_SONG, payload: info.data })
     }
 }
+
+export const postSong = (song) => {
+    return async function () {
+        let json = await axios.post(`${URL}/song/create`, song, {headers: {'Content-Type': 'multipart/form-data'}})
+        return json;
+    }
+}
+export const deleteSong = (ID) => {
+    
+    return async function () {
+        let json = await axios.post(`${URL}/song/delete`, {id:ID})
+        return json;
+    }
+}
+
+// ------------------ FILTROS --------------------------------------
+
+export const filtroGenero = (payload) => {
+    return {
+        type: FILTRO_GENERO,
+        payload
+    }
+}
+
+export const ordenPorAbc = (payload) => {
+    return {
+        type: ORDEN_BY_NAME,
+        payload
+    }
+}
+
+// ------------------ REPRODUCTOR --------------------------------------
+
+export const getPlayer = (payload) => {
+    return {
+        type: GET_PLAYER,
+        payload
+    }
+}
+
+export const isPlaying = () => {
+    return {
+        type: IS_PLAYING
+    }
+}
+
+// ------------------ OTROS --------------------------------------
+
+export const Landing = () => {
+    return {
+        type: "LANDING"
+    }
+}
+
+export function resetDetalles() {
+    return async function (dispatch) {
+        dispatch({ type: RESET_DETALLES })
+    }
+}
+
+
+
+
 
 
 
